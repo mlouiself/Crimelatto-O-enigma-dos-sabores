47a784ae011f4ba8955fc994152f7d9e2e22db5c
mlouiself <marinallfigueiredo@gmaul.comm>
2025-02-25 21:06:35 -0300

    Criação e definição de dados do csv

--- lib.py
+++ lib.py
@@ -19,6 +19,13 @@ def interrogar():
 
 
 
+import pandas as pd
+
+# Carregar o CSV corretamente configurado
+df = pd.read_csv("seu_arquivo.csv", delimiter=";")
+
+# Exibir as primeiras linhas para conferir
+print(df.head())
 
 
 
